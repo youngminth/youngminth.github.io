33fb0fea79a5c563c28b5ed8d9f903773daf073c
youngminth <skyisu123@gmail.com>
2022-08-25 01:02:19 +0900

    timestamp 변경

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: `zoomkoding.com`,
-  description: `줌코딩의 개발일기`,
+  title: `youngmin's blog`,
+  description: `송영민 (안드로이드 개발자)`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
   siteUrl: `https://www.zoomkoding.com`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
@@ -11,16 +11,16 @@ module.exports = {
   },
   ga: '0', // Google Analytics Tracking ID
   author: {
-    name: `정진혁`,
+    name: `송영민`,
     bio: {
       role: `개발자`,
-      description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
-      thumbnail: 'sample.png', // Path to the image in the 'asset' folder
+      description: ['더 좋은 사용자 경험을 제공하는', '더 나은 환경을 만드는', '늘 내가 만든 앱을 사용하는 사람들을 위해  끝 없이 고민하는'],
+      thumbnail: 'youngmin_emoji.gif', // Path to the image in the 'asset' folder
     },
     social: {
-      github: ``, // `https://github.com/zoomKoding`,
-      linkedIn: ``, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
-      email: ``, // `zoomkoding@gmail.com`,
+      github: `https://github.com/youngminth`,
+      linkedIn: `https://www.linkedin.com/in/영민-송-9b971218a`,
+      email: `skyisu123@gmail.com`,
     },
   },
 
@@ -43,13 +43,137 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        date: '2021.02 ~',
-        activity: '개인 블로그 개발 및 운영',
+        date: '2017.10.16 ~ 현재 재직',
+        activity: '(주)유성소프트 입사',
         links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
+          post: 'http://ussoft.co.kr/',
         },
+      },
+      // ========================================================
+      // ========================================================
+      {
+        date: '2017.11.16 ~ 2018.02.01',
+        activity: '(주)엔캣 WMS 앱 개발 및 운영',
+        links: {
+          post: 'https://n-cat.co.kr/',
+          googlePlay: 'http://pda.n-cat.kr/wms/',
+        },
+      },
+      // ========================================================
+      // ========================================================
+      {
+        date: '2017.12.01 ~ 2018.04.01',
+        activity: '(주)위비스 RFID 앱 개발 및 운영',
+        links: {
+          googlePlay: 'http://mob.wivis.com/wms/',
+        },
+      },
+      // ========================================================
+      // ========================================================
+      {
+        date: '2018.02.01 ~ 2018.07.01',
+        activity: '(주)케이투코리아 RFID 앱 개발 및 운영',
+        links: {
+          post: 'https://k2group.co.kr/main/',
+          googlePlay: 'http://mob.k2.co.kr/shop/',
+        },
+      },
+      // ========================================================
+      // ========================================================
+      {
+        date: '2018.05.01 ~ 2018.07.01',
+        activity: '(주)리앤한 RFID 앱 개발 및 운영',
+        links: {
+          post: 'http://www.leenhan.com/',
+          googlePlay: 'http://mob.leenhan.com/shop/',
+        },
+      },
+      // ========================================================
+      // ========================================================
+      {
+        date: '2019.04.01 ~ 2019.06.01',
+        activity: '(주)이랜드 스파오 매장 재고조회 키오스크 앱 개발',
+        links: {
+          post: 'https://www.paxetv.com/news/articleView.html?idxno=95836/',
+          demo: 'https://youtu.be/t05RfAmxU8c/',
+        },
+      },
+      // ========================================================
+      // ========================================================
+      {
+        date: '2019.12.01 ~ 2020.06.01',
+        activity: '(주)에프엔에프 RFID 앱 개발 및 운영',
+        links: {
+          post: 'https://www.fnf.co.kr/pages/main/main.php/',
+          googlePlay: 'https://rfid.fnf.co.kr/apk/shop/',
+        },
+      },
+      // ========================================================
+      // ========================================================
+      {
+        date: '2019.12.01 ~ 2020.06.01',
+        activity: '(주)삼성물산 패션부문 8S RFID 앱 개발 및 운영',
+        links: { },
+      },
+      // ========================================================
+      // ========================================================
+      {
+        date: '2020.11.01 ~ 2020.12.01',
+        activity: '사내 C/S RFID 웹 어플리케이션 개발',
+        links: {
+          post: '',
+          demo: '',
+        },
+      },
+      // ========================================================
+      // ========================================================
+      {
+        date: '2020.12.01 ~ 2021.02.01',
+        activity: '한국전화번호부 통화 종료 후 자동 SMS/MMS 발송 앱 개발',
+        links: {
+          googlePlay: 'https://m.onestore.co.kr/mobilepoc/apps/appsDetail.omp?prodId=0000755577/'
+        },
+      },
+      // ========================================================
+      // ========================================================
+      {
+        date: '2021.03.01 ~ 2021.08.01',
+        activity: '(주)한성에프아이 RFID 앱 개발 및 운영',
+        links: {
+          googlePlay: 'http://rfid.hansungfi.co.kr/apk/shop/'
+        },
+      },
+      // ========================================================
+      // ========================================================
+      {
+        date: '2021.08.01 ~ 2021.12.01',
+        activity: '밥상EIS 크로스 플랫폼 앱 개발',
+        links: {
+          googlePlay: 'https://was.ubob.biz/eis/apk/BobsangEIS/',
+          appStore: 'https://apps.apple.com/kr/app/밥상eis/id1620518213/',
+          demo: 'https://youtu.be/OM-ee5LL7SI/'
+        },
+      },
+      // ========================================================
+      // ========================================================
+      {
+        date: '2022.02.01 ~ 2022.03.01',
+        activity: '(주)대현 WMS 앱 개발',
+        links: {
+          googlePlay: 'https://cloud.bobsang.biz/mws/apkDown/daehyun/wms/',
+        },
+      },
+      // ========================================================
+      // ========================================================
+      {
+        date: '2022.03.01 ~ 2022.04.01',
+        activity: '(주)네파 WMS 앱 개발',
+      },
+      // ========================================================
+      // ========================================================
+      {
+        date: '2022.07.01 ~ 2022.10.01',
+        activity: '(주)독립문 WMS 앱 개발',
       },
     ],
 
